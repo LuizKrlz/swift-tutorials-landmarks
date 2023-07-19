c5d71384cef776ad1519b844e4990bcc8228d5b8
Luiz Correa <luiz.correa@laborit.com.br>
2023-07-19 05:55:21 -0300

    Initial Commit

---
+++ MeuAppTeste/MeuAppTesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeuAppTesteApp.swift
+//  MeuAppTeste
+//
+//  Created by Luiz Carlos Rocha Correa on 19/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MeuAppTesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
